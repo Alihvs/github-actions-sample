4e68f6d34c23a1c9578b59fd13ff42d9427f7384
Ali havasi <alihvs@gmail.com>
2020-08-05 14:18:36 +0430

    initial

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const dayOfTheWeek = (date = new Date()) => {
+
+    const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+
+    return days[date.getDay()];
+}
+
+try {
+    const element = document.getElementById('day');
+    if (element) element.innerText = dayOfTheWeek();
+} catch(err) {
+    console.log(err);
+}
+
+exports.dayOfTheWeek = dayOfTheWeek;
